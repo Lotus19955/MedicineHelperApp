c3f2e7333880c12db0e24b3f162e63a9bdc03820
Андрей Ермолович <lotus19955@gmail.com>
2022-12-14 01:20:08 +0300

    Added search period for fluorography

---
+++ MedicineHelper.Data.Abstractions/IUnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using MedicineHelper.Data.Abstractions.Repositories;
+using MedicineHelper.DataBase.Entities;
+
+namespace MedicineHelper.Data.Abstractions
+{
+    public interface IUnitOfWork
+    {
+
+        IRepository<Conclusion> Conclusion { get; }
+        IRepository<Appointment> Appointment { get; }
+        IRepository<Disease> Disease { get; }
+        IRepository<Doctor> Doctor { get; }
+        IRepository<DoctorVisit> DoctorVisit { get; }
+        IRepository<Fluorography> Fluorography { get; }
+        IRepository<MedicineСheckup> MedicineСheckup { get; }
+        IRepository<Clinic> Clinic { get; }
+        IRepository<Medicine> Medicine { get; }
+        IRepository<MedicineProcedure> MedicineProcedure { get; }
+        IRepository<MedicinePrescription> MedicinePrescription { get; }
+        IRepository<DiseaseHistory> DiseaseHistory { get; }
+        IRepository<Vaccination> Vaccination { get; }
+
+        IRepository<User> User { get; }
+        IRepository<Role> Role { get; }
+
+        Task<int> Commit();
+    }
+}
